81d31ef971e356f79aaefa279a41c7ae6b88dcd7
Masaya Ogushi <snow.akogi.pgel@gmail.com>
2015-10-31 11:46:09 +0900

    first

---
+++ setup_wrap.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+ext = Extension(name="wrap_fib", sources=["cfib.c", "wrap_fib.pyx"])
+
+setup(ext_modules=cythonize(ext))
