4f097fade8b213752769003d92b81e170f7fd4c1
Masaya Ogushi <snow.akogi.pgel@gmail.com>
2015-10-31 12:02:17 +0900

    custimize setup.py

--- setup_wrap.py
+++ setup_wrap.py
@@ -3,6 +3,9 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-ext = Extension(name="wrap_fib", sources=["cfib.c", "wrap_fib.pyx"])
+ext = Extension(name="wrap_fib", 
+        sources = ["wrap_fib.pyx"], 
+        library_dirs = ["/path/to/libfib.so"],
+        libraries = ["fib"])
 
 setup(ext_modules=cythonize(ext))
